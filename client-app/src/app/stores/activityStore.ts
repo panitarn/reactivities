89f8483d41373817331a1b86b32656a044ecca8f
panitarn <panitarn.work@gmail.com>
2021-08-13 22:02:50 +0700

    styling user interface

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -13,9 +13,19 @@ export default class ActivityStore {
         makeAutoObservable(this)
     }
 
-    get activityByDate() {
+    get activitiesByDate() {
         return Array.from(this.activityRegistry.values()).sort((a, b) => 
             Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+            }, {} as {[key: string]: Activity[]})
+        )
     }
 
     loadActivities = async () => {
